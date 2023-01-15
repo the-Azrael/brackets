99d3ea77d4d80b0c6b5b37da7f9ca26efc4c6fc8
Maksim Tusaev <tusaev.maxim@yandex.ru>
2023-01-15 18:02:40 +0400

    resolve in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let typeBracket = [];
+  let lifo = [];
+  
+  if (str.length % 2 !== 0)
+    return false;
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    typeBracket.push( { 
+      opnBrText: bracketsConfig[i][0],
+      clsBrText: bracketsConfig[i][1]
+    });
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let fndOpened = (typeBracket.find(op => str[i] === op.opnBrText)); 
+    let fndClosed = (typeBracket.find(op => str[i] === op.clsBrText));
+
+    if (fndOpened !== undefined) {
+      if (fndOpened === fndClosed) { 
+        if (fndOpened.opnBrText !== lifo[lifo.length-1])
+          lifo.push(fndOpened.clsBrText);
+        else
+          lifo.pop();
+      }
+      else {
+        lifo.push(fndOpened.clsBrText);
+      }
+    }
+    else {
+      if (fndClosed != undefined) {
+        if (str[i] !== lifo[lifo.length-1]) {
+          return false;
+        }
+        else {
+          lifo.pop();
+        }  
+      }
+      else
+        return false;
+    }      
+  }
+  if (lifo.length > 0)
+    return false;
+  
+  return true;
 }
